46c3f318ba3b80d62aed1c970a43934632e5ea58
shubham4132 <shubhamg3122134@gmail.com>
2025-08-30 12:32:37 +0530

    completed working on the product-details controllers

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 
+const shopProductsRouter = require("./routes/shop/products-routes");
+
 //connect to the database
 mongoose
   .connect(
@@ -35,6 +37,7 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 
+app.use("/api/shop/products", shopProductsRouter);
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
 });
