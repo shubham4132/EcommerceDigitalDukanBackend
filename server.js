5041d42add6213e224cc2f602cf171931656aa9b
shubham4132 <shubhamg3122134@gmail.com>
2025-12-16 18:00:04 +0530

    implemented cart logic address logic

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 
 const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
 
 //connect to the database
 mongoose
@@ -32,12 +34,16 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 
 app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
 });
